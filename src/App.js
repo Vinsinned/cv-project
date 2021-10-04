8060b9e8867ecb9bad5e430f9984a000504b5fe7
vinson <dagger1245653@gmail.com>
2021-10-03 18:14:29 -0700

    add basic input form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import react from "react";
+import ReactDOM from "react-dom";
+import './styles/App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="form">
+      <form>
+        <label htmlFor="name" className="makeBlock">Name</label>
+        <input type="text" id="name" className="makeBlock input"/>
+        <label htmlFor="name" className="makeBlock">Email</label>
+        <input type="text" id="email" className="makeBlock input"/>
+        <label htmlFor="name" className="makeBlock">Phone number</label>
+        <input type="text" id="phone" className="makeBlock input"/>
+        
+        <label htmlFor="name" className="makeBlock">School name</label>
+        <input type="text" id="school" className="makeBlock input"/>
+        <label htmlFor="name" className="makeBlock">Major</label>
+        <input type="text" id="major" className="makeBlock input"/>
+        <label htmlFor="name" className="makeBlock">Date of graduation</label>
+        <input type="text" id="date" className="makeBlock input"/>
+
+        <label htmlFor="name" className="makeBlock">Previous company</label>
+        <input type="text" id="company" className="makeBlock input"/>
+        <label htmlFor="name" className="makeBlock">Position</label>
+        <input type="text" id="position" className="makeBlock input"/>
+        <label htmlFor="name" className="makeBlock">Main tasks</label>
+        <input type="text" id="task" className="makeBlock input"/>
+        <label htmlFor="name" className="makeBlock">How long did you work</label>
+        <input type="text" id="duration" className="makeBlock input"/>
+
+        <button type="submit" className="makeBlock">
+          Submit
+        </button>
+      </form>
     </div>
   );
 }
